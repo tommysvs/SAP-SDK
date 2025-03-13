f7f4a46f0d8dffc08edc8d35569624c8e9892e3f
Tommy Vega <tvega@grupoplatino.hn>
2025-03-13 15:22:38 -0600

    Se crean clases para los asientos

---
+++ BE/csJDT1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class csJDT1
+    {
+        public string Account { get; set; }
+        public string ShortName { get; set; }
+        public string Debit { get; set; }
+        public string Credit { get; set; }
+        public string FCDebit { get; set; }
+        public string FCCredit { get; set; }
+        public string FCCurrency { get; set; }
+        public string Project { get; set; }
+        public string OcrCode1 { get; set; }
+        public string OcrCode2 { get; set; }
+        public string OcrCode3 { get; set; }
+        public string OcrCode4 { get; set; }
+        public string OcrCode5 { get; set; }
+    }
+}
---
+++ BE/csOJDT.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class csOJDT
+    {
+        public int? JdtNum { get; set; }
+        public string RefDate { get; set; }
+        public string DueDate { get; set; }
+        public string TaxDate { get; set; }
+        public string Memo { get; set; }
+        public string Indicator { get; set; }
+        public string Project { get; set; }
+        public string Transcode { get; set; }
+        public string Ref1 { get; set; }
+        public string Ref2 { get; set; }
+        public string Ref3 { get; set; }
+
+        public List<csJDT1> ListDet = new List<csJDT1>();
+    }
+}
